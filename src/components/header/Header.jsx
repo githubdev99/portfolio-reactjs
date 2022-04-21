dfb9c4f72df331f62bb3039b05f9af1a5deb0dbd
DEVAN FIRMANSYAH RAMADHAN <devanramadhan92@gmail.com>
2022-04-21 15:59:24 +0700

    [UPDATE] create component Header, About, Contact, Experience, Nav, Portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import './header.css'
 import CTA from './CTA';
 import ME from './../../assets/me-edited.png'
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
     return (
         <header>
             <div className="container header__container">
-                <h5>Hello I'm</h5>
+                <h5>Hi there, I'm</h5>
                 <h1>Devan Firmansyah Ramadhan</h1>
                 <h5 className="text-light">Web Developer</h5>
                 <CTA />
+                <HeaderSocials />
 
                 <div className="me">
                     <img src={ME} alt="me" />
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
 
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://linkedin.com" target="_blank" rel='noreferrer'></a>
+            <a href="https://www.linkedin.com/in/devan-firmansyah-ramadhan-aaba4a10a" target="_blank" rel='noreferrer'><BsLinkedin /></a>
+            <a href="https://github.com/githubdev99" target="_blank" rel='noreferrer'><BsGithub /></a>
+            <a href="https://www.instagram.com/devanfrmnsyh/" target="_blank" rel='noreferrer'><BsInstagram /></a>
         </div>
     )
 }
