9c8cba60fd583f995eadf455cfacb31db542afab
DEVAN FIRMANSYAH RAMADHAN <devanramadhan92@gmail.com>
2022-04-22 13:41:08 +0700

    [UPDATE] adding nav portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
 import './nav.css'
-import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineHome, AiOutlineUser, AiOutlineFundProjectionScreen } from 'react-icons/ai'
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi'
 import { RiServiceLine } from 'react-icons/ri'
 import { MdOutlineWork } from 'react-icons/md'
@@ -15,6 +15,7 @@ const Nav = () => {
             <a href="#work" onClick={() => setActiveNav('#work')} className={(activeNav === '#work') ? 'active' : ''}><MdOutlineWork /></a>
             <a href="#experience" onClick={() => setActiveNav('#experience')} className={(activeNav === '#experience') ? 'active' : ''}><BiBook /></a>
             <a href="#services" onClick={() => setActiveNav('#services')} className={(activeNav === '#services') ? 'active' : ''}><RiServiceLine /></a>
+            <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={(activeNav === '#portfolio') ? 'active' : ''}><AiOutlineFundProjectionScreen /></a>
             <a href="#contact" onClick={() => setActiveNav('#contact')} className={(activeNav === '#contact') ? 'active' : ''}><BiMessageSquareDetail /></a>
         </nav>
     )
