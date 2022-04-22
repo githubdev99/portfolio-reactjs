bbe82292ac5d3a950adde0dc2d79631ce0131146
DEVAN FIRMANSYAH RAMADHAN <devanramadhan92@gmail.com>
2022-04-22 13:30:00 +0700

    [UPDATE] adding json

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from './components/about/About';
 import Experience from './components/experience/Experience';
 import Services from './components/services/Services';
 import Portfolio from './components/portfolio/Portfolio';
+import Work from './components/work/Work';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
@@ -13,6 +14,7 @@ import aboutData from './json/about.json'
 import experienceData from './json/experience.json'
 import servicesData from './json/services.json'
 import portfolioData from './json/portfolio.json'
+import workData from './json/work.json'
 
 const App = () => {
     return (
@@ -20,6 +22,7 @@ const App = () => {
             <Header profileData={profileData} />
             <Nav profileData={profileData} />
             <About profileData={profileData} aboutData={aboutData} />
+            <Work profileData={profileData} workData={workData} />
             <Experience profileData={profileData} experienceData={experienceData} />
             <Services profileData={profileData} servicesData={servicesData} />
             <Portfolio profileData={profileData} portfolioData={portfolioData} />
---
+++ src/components/work/Work.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './../portfolio/portfolio.css'
+import './work.css'
+
+const Work = (props) => {
+    return (
+        <section id='portfolio'>
+            <h5>Pernah Bekerja Di</h5>
+            <h2>Riwayat Pekerjaan</h2>
+
+            <div className="container portfolio__container">
+                {props.workData.cards.map((item, index) => {
+                    return (
+                        <article key={index} className='portfolio__item'>
+                            <div className="portfolio__item-image">
+                                <img src={item.image} alt={`img${index}`} />
+                            </div>
+                            <h3>{item.title}</h3>
+                            <p>{item.description}</p>
+                            <br />
+                            <div className="portfolio__item-cta">
+                                {(item.urlGithub) ? <a href={item.urlGithub} className='btn' target='_blank' rel='noreferrer'>Github</a> : ``}
+                                {(item.urlDemo) ? <a href={item.urlDemo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a> : ``}
+                            </div>
+                        </article>
+                    )
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Work
